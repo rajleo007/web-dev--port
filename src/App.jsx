fde7e69cb5fc467ca28dc9f4a64b9f95d0bab782
rajdip dey <rajdipdey97rd@gamil.com>
2024-05-18 14:20:47 +0530

    this is a portfolio

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Navbar from "./component/NavBar"
+import Home from "./component/Home"
+import Sociallinks from "./component/Sociallinks"
+import About from "./component/About"
+import Portfolio from "./component/Portfolio"
+import Experience from "./component/Experience"
+import Contact from "./component/Contact"
+
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <Navbar/>
+      <Home/>
+      <Sociallinks/>
+      <About/>
+      <Portfolio/>
+      <Experience/>
+      <Contact/>
+    </div>
+      
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/component/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg- bg-gradient-to-b from-gray-800 to-black text-white '>
+      <div  className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8 '>
+        <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+        </div>
+        <p className='text-xl mt-20 '>
+        Hello! I'm a passionate full-stack web developer with a knack for crafting dynamic and user-friendly digital experiences. Proficient in a range of technologies including HTML, CSS, React, Node.js, and various databases, I thrive on turning innovative ideas into functional, polished websites and applications. Beyond coding, you'll often find me on the football field or cricket pitch, where I channel my competitive spirit and teamwork skills. I'm continuously expanding my knowledge base, eagerly embracing new technologies and approaches to stay at the forefront of the ever-evolving web development landscape. Let's connect and bring your digital visions to life!
+
+
+
+
+
+        </p>
+      </div>
+
+    </div>
+  )
+}
+
+export default About
